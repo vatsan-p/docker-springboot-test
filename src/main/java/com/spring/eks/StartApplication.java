0af937871c1521c1c7ba7e1ef0880d8c2b37544b
vatsan-p <vatsan.santhosh@gmail.com>
2026-03-30 08:38:43 +0530

    Created Helm chart

---
+++ src/main/java/com/spring/eks/StartApplication.java
@@ -0,0 +1,24 @@
+package com.spring.eks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class StartApplication {
+
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "Welcome folks..We are learning Kubernete Deployment using Helm, Jenkins Pipeline Today's date is Jan 3rd, 2026");
+        model.addAttribute("msg", "Hello All..We are deploying springboot Microservice App into EKS cluster using Helm + Jenkins Pipeline!!!!");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
